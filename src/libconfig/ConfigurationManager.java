a3692a10d8703c36e2874b21414355e529d8d9ca
Iber <iberparodisiri@gmail.com>
2017-09-19 23:23:06 -0300

    Changed key descriptions to lowercase so you can copy & paste them to the .conf file. Removed ffmpeg, ffprobe and melt path's as are all the same and added mlt_framework_path instead.

--- src/libconfig/ConfigurationManager.java
+++ src/libconfig/ConfigurationManager.java
@@ -74,20 +74,17 @@ public class ConfigurationManager {
      * The FPS of all medias loaded in the system.
      * Note that if you change this you'll have to reload all your medias and pieces with the new configuration.
      */    
-    
     private static final String MEDIAS_FPS = "medias_fps";
     
     /**
      * MP-Devourer
      */
+    private static final String MLT_FRAMEWORK_DIR = "mlt_framework_dir";
     private static final String DEVOURER_INPUT_DIR = "devourer_input_dir";
     private static final String DEVOURER_OUTPUT_DIR = "devourer_output_dir";
     private static final String DEVOURER_MEDIA_DIR = "devourer_media_dir";
     private static final String DEVOURER_THUMB_DIR = "devourer_thumb_dir";
-    private static final String DEVOURER_FFMPEG_PATH = "devourer_ffmpeg_path";
-    private static final String DEVOURER_FFPROBE_PATH = "devourer_ffprobe_path";
-    private static final String DEVOURER_MELT_PATH = "devourer_melt_path";
-
+    
     private Properties properties;
 
     private ConfigurationManager(){
@@ -167,13 +164,10 @@ public class ConfigurationManager {
         
         p.setProperty(DEVOURER_INPUT_DIR, "EDIT ME!--> ~/Videos/input");
         p.setProperty(DEVOURER_OUTPUT_DIR, "EDIT ME!--> ~/Videos/output");
-        p.setProperty(DEVOURER_MEDIA_DIR, "EDIT ME!--> ~/Videos/");
+        p.setProperty(DEVOURER_MEDIA_DIR, ""); // Not used at the moment. Possiblly to store a remote path
         p.setProperty(DEVOURER_THUMB_DIR, "EDIT ME!--> /XXX/mp-installer/magma-playout/gui/mp-ui-playout/src/assets/img");
-        p.setProperty(DEVOURER_FFMPEG_PATH, "EDIT ME!--> /XXX/mp-installer//MagmaPlayout/core/melted/XXXXXXX/bin/ffmpeg");
-        p.setProperty(DEVOURER_FFPROBE_PATH, "EDIT ME!--> /XXX/mp-installer//MagmaPlayout/core/melted/XXXXXXX/bin/ffprobe");
-        p.setProperty(DEVOURER_MELT_PATH, "EDIT ME!--> /XXX/mp-installer//MagmaPlayout/core/melted/XXXXXXX/bin/melt");
+        p.setProperty(MLT_FRAMEWORK_DIR, "EDIT ME!--> /XXX/mp-installer//MagmaPlayout/core/melted/XXXXXXX/bin/ffmpeg");
         
-
         return p;
     }
 
@@ -274,18 +268,9 @@ public class ConfigurationManager {
         return properties.getProperty(DEVOURER_THUMB_DIR);
     }
 
-    public String getDevourerFfmpegPath() {
-        return properties.getProperty(DEVOURER_FFMPEG_PATH);
+    public String getMltFrameworkPath() {
+        return properties.getProperty(MLT_FRAMEWORK_DIR);
     }
-
-    public String getDevourerFfprobePath() {
-        return properties.getProperty(DEVOURER_FFPROBE_PATH);
-    }
-
-    public String getDevourerMeltPath() {
-        return properties.getProperty(DEVOURER_MELT_PATH);
-    }
-    
     
     
     /**
@@ -297,34 +282,32 @@ public class ConfigurationManager {
     public void printConfig(Logger logger){
         logger.log(Level.INFO,
             "Loaded configuration:\n"
-            +"\n\tCONFIG_PATH: " + ConfigurationManager.CONFIG_PATH
-            +"\n\tREDIS_HOST_KEY: " + properties.getProperty(REDIS_HOST_KEY)
-            +"\n\tREDIS_PORT_KEY: " + properties.getProperty(REDIS_PORT_KEY)
-            +"\n\tREDIS_PCCP_CHANNEL_KEY: " + properties.getProperty(REDIS_PCCP_CHANNEL_KEY)
-            +"\n\tREDIS_FSCP_CHANNEL_KEY: " + properties.getProperty(REDIS_FSCP_CHANNEL_KEY)
-            +"\n\tREDIS_PCR_CHANNEL_KEY: " + properties.getProperty(REDIS_PCR_CHANNEL_KEY)
-            +"\n\tREDIS_MSTA_CHANNEL_KEY: " + properties.getProperty(REDIS_MSTA_CHANNEL_KEY)
-            +"\n\tREDIS_RECONNECTION_TIMEOUT_KEY: " + properties.getProperty(REDIS_RECONNECTION_TIMEOUT_KEY)
-            +"\n\tMELTED_HOST_KEY: " + properties.getProperty(MELTED_HOST_KEY)
-            +"\n\tMELTED_PORT_KEY: " + properties.getProperty(MELTED_PORT_KEY)
-            +"\n\tMELTED_RECONNECTION_TIMEOUT_KEY: " + properties.getProperty(MELTED_RECONNECTION_TIMEOUT_KEY)
-            +"\n\tMELTED_RECONNECTION_TRIES_KEY: " + properties.getProperty(MELTED_RECONNECTION_TRIES_KEY)
-            +"\n\tMELTED_PLAYLIST_MAX_DURATION: " + properties.getProperty(MELTED_PLAYLIST_MAX_DURATION)
-            +"\n\tMELTED_APPENDER_WORKER_FREQ: " + properties.getProperty(MELTED_APPENDER_WORKER_FREQ)
-            +"\n\tMELT_PATH_KEY: " + properties.getProperty(MELT_PATH_KEY)
-            +"\n\tDEFAULT_MEDIA_PATH: " + properties.getProperty(DEFAULT_MEDIA_PATH)
-            +"\n\tMLT_SPACERS_PATH: " + properties.getProperty(MLT_SPACERS_PATH)
-            +"\n\tFILTER_SERVER_URL_KEY: " + properties.getProperty(FILTER_SERVER_URL_KEY)
-            +"\n\tBASH_TIMEOUT_KEY: " + properties.getProperty(BASH_TIMEOUT_KEY)
-            +"\n\tREST_BASE_URL: " + properties.getProperty(PLAYOUT_API_URL)
-            +"\n\tMEDIAS_FPS: " + properties.getProperty(MEDIAS_FPS)
-            +"\n\tDEVOURER_INPUT_DIR: " + properties.getProperty(DEVOURER_INPUT_DIR)
-            +"\n\tDEVOURER_OUTPUT_DIR: " + properties.getProperty(DEVOURER_OUTPUT_DIR)
-            +"\n\tDEVOURER_MEDIA_DIR: " + properties.getProperty(DEVOURER_MEDIA_DIR)
-            +"\n\tDEVOURER_THUMB_DIR: " + properties.getProperty(DEVOURER_THUMB_DIR)
-            +"\n\tDEVOURER_FFMPEG_PATH: " + properties.getProperty(DEVOURER_FFMPEG_PATH)
-            +"\n\tDEVOURER_FFPROBE_PATH: " + properties.getProperty(DEVOURER_FFPROBE_PATH)        
-            +"\n\tDEVOURER_MELT_PATH: " + properties.getProperty(DEVOURER_MELT_PATH)        
+            +"\n\tconfig_path: " + ConfigurationManager.CONFIG_PATH
+            +"\n\tredis_host_key: " + properties.getProperty(REDIS_HOST_KEY)
+            +"\n\tredis_port_key: " + properties.getProperty(REDIS_PORT_KEY)
+            +"\n\tredis_pccp_channel_key: " + properties.getProperty(REDIS_PCCP_CHANNEL_KEY)
+            +"\n\tredis_fscp_channel_key: " + properties.getProperty(REDIS_FSCP_CHANNEL_KEY)
+            +"\n\tredis_pcr_channel_key: " + properties.getProperty(REDIS_PCR_CHANNEL_KEY)
+            +"\n\tredis_msta_channel_key: " + properties.getProperty(REDIS_MSTA_CHANNEL_KEY)
+            +"\n\tredis_reconnection_timeout_key: " + properties.getProperty(REDIS_RECONNECTION_TIMEOUT_KEY)
+            +"\n\tmelted_host_key: " + properties.getProperty(MELTED_HOST_KEY)
+            +"\n\tmelted_port_key: " + properties.getProperty(MELTED_PORT_KEY)
+            +"\n\tmelted_reconnection_timeout_key: " + properties.getProperty(MELTED_RECONNECTION_TIMEOUT_KEY)
+            +"\n\tmelted_reconnection_tries_key: " + properties.getProperty(MELTED_RECONNECTION_TRIES_KEY)
+            +"\n\tmelted_playlist_max_duration: " + properties.getProperty(MELTED_PLAYLIST_MAX_DURATION)
+            +"\n\tmelted_appender_worker_freq: " + properties.getProperty(MELTED_APPENDER_WORKER_FREQ)
+            +"\n\tmelt_path_key: " + properties.getProperty(MELT_PATH_KEY)
+            +"\n\tdefault_media_path: " + properties.getProperty(DEFAULT_MEDIA_PATH)
+            +"\n\tmlt_spacers_path: " + properties.getProperty(MLT_SPACERS_PATH)
+            +"\n\tfilter_server_url_key: " + properties.getProperty(FILTER_SERVER_URL_KEY)
+            +"\n\tbash_timeout_key: " + properties.getProperty(BASH_TIMEOUT_KEY)
+            +"\n\trest_base_url: " + properties.getProperty(PLAYOUT_API_URL)
+            +"\n\tmedias_fps: " + properties.getProperty(MEDIAS_FPS)
+            +"\n\tdevourer_input_dir: " + properties.getProperty(DEVOURER_INPUT_DIR)
+            +"\n\tdevourer_output_dir: " + properties.getProperty(DEVOURER_OUTPUT_DIR)
+            //+"\n\tdevourer_media_dir: " + properties.getProperty(DEVOURER_MEDIA_DIR) // Not implemented
+            +"\n\tdevourer_thumb_dir: " + properties.getProperty(DEVOURER_THUMB_DIR)
+            +"\n\tmlt_framework_dir: " + properties.getProperty(MLT_FRAMEWORK_DIR)
             +"\n"
         );
     }
